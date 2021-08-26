d070502c6aaa1085b38015d1fba143a4997e51f2
takahiro23dol <t_nakazato@jwts.co.jp>
2021-08-26 09:30:54 +0900

    Molecules/Messageの作成

---
+++ src/stories/molecules-messages.stories.js
@@ -0,0 +1,5 @@
+import msg from "../components/molecules/messages/messages.html";
+import "../components/molecules/messages/messages.scss";
+
+export default { title: "molecules/messages" };
+export const message = () => msg;
